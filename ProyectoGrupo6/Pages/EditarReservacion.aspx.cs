e6ec24ee94ee28d3b5823305dc228cf47ace1ba3
JimeSof <donsosofia@gmail.com>
2025-11-27 20:55:45 -0600

    Cambios a realizar en editar

--- ProyectoGrupo6/Pages/EditarReservacion.aspx.cs
+++ ProyectoGrupo6/Pages/EditarReservacion.aspx.cs
@@ -3,14 +3,14 @@ using ProyectoGrupo6.Classes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Web.Caching;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace ProyectoGrupo6.Pages
 {
     public partial class EditarReservacion : System.Web.UI.Page
     {
+
         protected void Page_Load(object sender, EventArgs e)
         {
             //Permite cargar los datos en los campos usando un procedimiento almacenado
@@ -20,39 +20,127 @@ namespace ProyectoGrupo6.Pages
                 try
                 {
                     Usuario usuario = (Usuario)Session["Usuario"];
-
-                    int id = int.Parse(Request.QueryString["idReservacion"]);
-
-                    int idPersona = int.Parse(usuario.idPersona.ToString());
                     bool esEmpleado = Convert.ToBoolean(Session["esEmpleado"]);
 
-                    using (PvProyectoFinalDB db = new PvProyectoFinalDB("Database"))
-                    {
-                        var editReservacion = db.SpObtenerReservacionById(id, idPersona, esEmpleado).FirstOrDefault();
-
-                        if (editReservacion != null)
-                        {
-                            txtHotel.Text = editReservacion.Hotel;
-                            txtNumeroHabitacion.Text = editReservacion.NumeroHabitacion.ToString();
-                            txtCliente.Text = editReservacion.Cliente;
-                            txtFechaEntrada.Text = editReservacion.FechaEntrada.ToString("yyyy-MM-dd");
-                            txtFechaSalida.Text = editReservacion.FechaSalida.ToString("yyyy-MM-dd");
-                            txtNumeroAdultos.Text = editReservacion.NumeroAdultos.ToString();
-                            txtNumeroNinhos.Text = editReservacion?.NumeroNinhos.ToString();
-                        }
-                        else
-                        {
-                            Response.Redirect("~/Pages/Detalle.aspx");
-                        }
-
-
-                    }
+                    CargarDatos(usuario, esEmpleado);
 
                 }
                 catch { }
 
             }
 
+                
+        }
+
+        private void CargarDatos(Usuario usuario, bool esEmpleado)
+        {
+            //este public lo que hace es cargar los datos de la reservacion seleccionada
+            int id = int.Parse(Request.QueryString["idReservacion"]);
+
+            using (PvProyectoFinalDB db = new PvProyectoFinalDB("Database"))
+            {
+                var data = db.SpObtenerReservacionById(id, usuario.idPersona.Value, esEmpleado).FirstOrDefault();
+
+                DateTime hoy = DateTime.Now;
+
+                //verifica si la reservacion esta inactiva o si la fecha de salida ya paso o si la fecha de entrada es hoy o ya paso y el usuario no es empleado
+                //en ese caso redirige segun el tipo de usuario y buscar entrar por url
+                if (data.Estado == 'I' || data.FechaSalida <= hoy || (data.FechaEntrada <= hoy && data.FechaSalida > hoy && !esEmpleado))
+                {
+                    RedirigirSegunUsuario(esEmpleado);
+                    return;
+                }
+
+                //llena los campos con los datos obtenidos
+                hfnIdReservacion.Value = data.IdReservacion.ToString();
+                txtHotel.Text = data.Hotel;
+                txtNumeroHabitacion.Text = data.NumeroHabitacion;
+                txtCliente.Text = data.Cliente;
+
+                txtFechaEntrada.Text = data.FechaEntrada.ToString("yyyy-MM-dd");
+                txtFechaSalida.Text = data.FechaSalida.ToString("yyyy-MM-dd");
+                txtNumeroAdultos.Text = data.NumeroAdultos.ToString();
+                txtNumeroNinhos.Text = data.NumeroNinhos.ToString();
+            }
+        }
+
+        private void RedirigirSegunUsuario(bool esEmpleado)
+        {
+            //redirecciona segun el tipo de usuario
+            if (esEmpleado)
+                Response.Redirect("GestionarReservaciones.aspx");
+            else
+                Response.Redirect("MisReservaciones.aspx");
+        }
+
+        protected void btnGuardar_Click(object sender, EventArgs e)
+        { //Permite guardar los cambios realizados en la reservacion usando un procedimiento almacenado
+            try
+            {
+                //LAS VALIDACIONES NO PUEDEN IR EN EL GUARDAR, DEBEN IR APARTE EN REQUIERIED FIELD VALIDATOR O CUSTOM VALIDATOR
+
+
+                if (string.IsNullOrWhiteSpace(txtFechaEntrada.Text) ||
+                    string.IsNullOrWhiteSpace(txtFechaSalida.Text) ||
+                    string.IsNullOrWhiteSpace(txtNumeroAdultos.Text) ||
+                    string.IsNullOrWhiteSpace(txtNumeroNinhos.Text))
+                {
+                    MostrarMensaje("Todos los campos son requeridos.");
+                    return;
+                }
+
+                DateTime entrada = DateTime.Parse(txtFechaEntrada.Text);
+                DateTime salida = DateTime.Parse(txtFechaSalida.Text);
+
+                if (entrada <= DateTime.Now || salida <= DateTime.Now)
+                {
+                    MostrarMensaje("Las fechas deben ser posteriores a hoy.");
+                    return;
+                }
+
+                if (salida <= entrada)
+                {
+                    MostrarMensaje("La fecha de salida debe ser mayor que la fecha de entrada.");
+                    return;
+                }
+
+                int adultos = int.Parse(txtNumeroAdultos.Text);
+                int ninhos = int.Parse(txtNumeroNinhos.Text);
+
+                if (adultos <= 0 || ninhos < 0)
+                {
+                    MostrarMensaje("Número de personas incorrecto.");
+                    return;
+                }
+
+                //HASTA AQUI LAS VALIDACIONS NO PUEDEN IR EN EL GUARDAR, DEBEN IR APARTE EN REQUIERIED FIELD VALIDATOR O CUSTOM VALIDATOR
+
+                //PON LAS VARIABLES AQUI PARA QUE NO HAYA PROBLEMAS AL MOMENTO DE GUARDAR
+
+
+                Usuario usuario = (Usuario)Session["Usuario"];
+
+                using (PvProyectoFinalDB db = new PvProyectoFinalDB("Database"))
+                {
+                  //  db.SpEditarReservacion(idReserva, entrada, salida, adultos, ninhos, usuario.idPersona.Value);
+                }
+
+                bool esEmpleado = Convert.ToBoolean(Session["esEmpleado"]);
+
+                if (esEmpleado)
+                    Response.Redirect("GestionarReservaciones.aspx");
+                else
+                    Response.Redirect("MisReservaciones.aspx");
+            }
+            catch
+            {
+                MostrarMensaje("Error al guardar la reservación.");
+            }
+        }
+
+        private void MostrarMensaje(string msg)
+        {
+            ScriptManager.RegisterStartupScript(this, GetType(), "alert", $"alert('{msg}');", true);
         }
 
 
@@ -66,14 +154,6 @@ namespace ProyectoGrupo6.Pages
 
             }
             catch { }
-        }
-
-        protected void btnGuardar_Click(object sender, EventArgs e)
-        {//Boton Guardar los cambios
-            try
-            {
-
-            }catch { }
         }
     }
 }
--- ProyectoGrupo6/Pages/EditarReservacion.aspx.designer.cs
+++ ProyectoGrupo6/Pages/EditarReservacion.aspx.designer.cs
@@ -96,6 +96,15 @@ namespace ProyectoGrupo6.Pages
         protected global::System.Web.UI.WebControls.TextBox txtFechaEntrada;
 
         /// <summary>
+        /// Control RequiredFieldValidator1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator1;
+
+        /// <summary>
         /// Control lblFechaSalida.
         /// </summary>
         /// <remarks>
